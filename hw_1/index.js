77b8baf2686f7e5fd2cab320f337bb7d54c29e9e
Daria Buhaiova <dashabugayova556@gmail.com>
2023-05-02 18:55:26 +0300

    1

---
+++ hw_1/index.js
@@ -0,0 +1,5 @@
+const numberOne = +prompt("Перше число");
+const numberTwo = +prompt("Друге число");
+const numberThree = +prompt("Третє число");
+const result = ((numberOne + numberTwo + numberThree)/3);
+alert ("Середнє арифметичне: " +result)
