4a548cbf9583e3c23a05029af346f736e96ea0e4
Daria Buhaiova <dashabugayova556@gmail.com>
2023-06-04 23:18:08 +0300

    Profile done

--- hw_12/index.js
+++ hw_12/index.js
@@ -1,20 +1,34 @@
-document.querySelector('#loginButton').addEventListener("click", function login() {
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
+document.querySelector("#loginButton").addEventListener("click", function login() {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    if (email === "" || password === "") {
+      alert("Введіть ваш логін і пароль");
+      return;
+    }
+
     console.log(`Email: ${email}`);
     console.log(`Password: ${password}`);
-  
+    document.getElementById("email").style.display = "none";
+    document.getElementById("password").style.display = "none";
+    document.getElementById("label").style.display = "none";
+    document.getElementById("label1").style.display = "none";
+    document.getElementById("loginButton").style.display = "none";
     spinner();
-  
-    setTimeout(function() {
-        document.getElementById("spinner").style.display = "none";
+
+    setTimeout(function () {
+      document.getElementById("spinner").style.display = "none";
+      window.location.href = "index2.html";
+      document.getElementById("email").style.display = "none";
     }, 2000);
   });
-  
-  function spinner() {
-    document.getElementById("spinner").style.display = "block";
-  }
-  
-  
+function spinner() {
+  document.getElementById("spinner").style.display = "block";
+}
 
-          
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.location.href.includes("index2.html")) {
+    console.log("Ширина екрану:", window.screen.width);
+    console.log("Висота екрану:", window.screen.height);
+    document.getElementById("loginButton").style.display = "none";
+  }
+});
